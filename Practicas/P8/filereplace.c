660661f43e66352d7a69e148c68cf419a0f57694
Miguel Cabral <miguelcbrm@gmail.com>
2018-04-09 18:35:51 -0500

    added P8

--- Practicas/P8/filereplace.c
+++ Practicas/P8/filereplace.c
@@ -33,6 +33,16 @@ int main(){
                 character = fgetc(input);
                 fputc(character, stdout);
             }
+            fputs(replace, stdout);
+            fseek(input, strlen(word), SEEK_CUR;
+            while(1){
+                character = fgetc(input);
+                if(feof(input)){
+                    break;
+                }
+                fputc(character, stdout);
+            }
+            break;
         }
     }
 }
